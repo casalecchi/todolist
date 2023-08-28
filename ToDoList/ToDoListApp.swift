1300b2f941ca7804d4f486fa9f476c63147b4106
casalecchi <felipevilela191@gmail.com>
2023-08-28 16:38:51 -0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Felipe Casalecchi on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
