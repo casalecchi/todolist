fc7bc1c2d8434df94e0e06cb42aceac534b5aaa2
casalecchi <felipevilela191@gmail.com>
2023-08-28 16:58:55 -0300

    add `title` attribute

--- ToDoList/Views/ListRowView.swift
+++ ToDoList/Views/ListRowView.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 struct ListRowView: View {
+    let title: String
+    
     var body: some View {
         HStack {
             Image(systemName: "checkmark.circle")
-            Text("This is the first item")
+            Text(title)
             Spacer()
         }
     }
@@ -19,6 +21,6 @@ struct ListRowView: View {
 
 struct ListRowView_Previews: PreviewProvider {
     static var previews: some View {
-        ListRowView()
+        ListRowView(title: "This is the first title")
     }
 }
